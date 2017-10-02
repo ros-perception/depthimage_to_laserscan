a5e602cb903caec2910023dfa3aed0d64774011b
Konrad Malkowski <kmalkowski@6river.com>
2017-10-02 10:26:34 -0400

    Optimize performance of vision subsystem

--- include/depthimage_to_laserscan/DepthImageToLaserScan.h
+++ include/depthimage_to_laserscan/DepthImageToLaserScan.h
@@ -166,6 +166,69 @@ namespace depthimage_to_laserscan
      */
     bool use_point(const float new_value, const float old_value, const float range_min, const float range_max) const;
 
+
+      // Polynomial approximating arctangenet on the range -1,1.
+// Max error < 0.005 (or 0.29 degrees)
+      double ApproxAtan(double z) const
+      {
+        const float n1 = 0.97239411f;
+        const float n2 = -0.19194795f;
+        return (n1 + n2 * z * z) * z;
+      }
+
+      double ApproxAtan2(double y, double x) const
+      {
+
+        if (x != 0.0f)
+        {
+          if (fabsf(x) > fabsf(y))
+          {
+            const double z = y / x;
+            if (x > 0.0)
+            {
+              // atan2(y,x) = atan(y/x) if x > 0
+              return ApproxAtan(z);
+            }
+            else if (y >= 0.0)
+            {
+              // atan2(y,x) = atan(y/x) + PI if x < 0, y >= 0
+              return ApproxAtan(z) + M_PI;
+            }
+            else
+            {
+              // atan2(y,x) = atan(y/x) - PI if x < 0, y < 0
+              return ApproxAtan(z) - M_PI;
+            }
+          }
+          else // Use property atan(y/x) = PI/2 - atan(x/y) if |y/x| > 1.
+          {
+            const double z = x / y;
+            if (y > 0.0)
+            {
+              // atan2(y,x) = PI/2 - atan(x/y) if |y/x| > 1, y > 0
+              return -ApproxAtan(z) + M_PI_2;
+            }
+            else
+            {
+              // atan2(y,x) = -PI/2 - atan(x/y) if |y/x| > 1, y < 0
+              return -ApproxAtan(z) - M_PI_2;
+            }
+          }
+        }
+        else
+        {
+          if (y > 0.0f) // x = 0, y > 0
+          {
+            return M_PI_2;
+          }
+          else if (y < 0.0f) // x = 0, y < 0
+          {
+            return -M_PI_2;
+          }
+        }
+        return 0.0f; // x,y = 0. Could return NaN instead.
+      }
+
     /**
     * Converts the depth image to a laserscan using the DepthTraits to assist.
     * 
